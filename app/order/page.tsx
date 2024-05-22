cd64603d9f668e1d713e549ebf032234635412c4
Eðvald Atli Sigurvaldsson <edvaldatli@hotmail.com>
2024-05-22 12:17:42 +0000

    build fix

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -44,14 +44,25 @@ export default function OrderPage({ orderEmail }: Props) {
 
   return (
     <div className="flex flex-col bg-request-orange w-full h-full rounded-xl p-4 text-white">
-      <h1 className="text-3xl font-bold text-center">Your order</h1>
-      <div className="flex flex-row w-2/3 justify-center">
-        <ul className="flex flex-col">
+      <h1 className="text-3xl font-bold text-center pb-2">Your order</h1>
+      <div className="flex flex-row w-full justify-center overflow-auto">
+        <ul className="flex flex-col gap-2 w-1/2 h-full">
           {orders.map((order) => (
-            <li className="flex flex-col bg-white rounded-xl text-black">
+            <li
+              className="flex flex-col bg-white rounded-xl text-black w-full p-4"
+              key={order.id}
+            >
               <h2>{order.orderTimestamp}</h2>
               <h3>{formatDate(order.time)}</h3>
-              <h3>{order.total}</h3>
+              <h3>Total: {order.total}</h3>
+              <li>
+                {order.dishes.map((dish) => (
+                  <li key={dish.dish.id}>
+                    <p>{dish.dish.name}</p>
+                    <p>{dish.dish.price}</p>
+                  </li>
+                ))}
+              </li>
             </li>
           ))}
         </ul>
--- app/utils/serverFunctions.ts
+++ app/utils/serverFunctions.ts
@@ -3,51 +3,63 @@ import PocketBase from "pocketbase";
 import { CocktailType, DishType } from "../types/types";
 
 export type OrderType = {
-    firstName: string;
-    email: string;
-    orderTimestamp: string;
-    time: string;
-    dishes: { dish: DishType; quantity: number }[];
-    drinks: { drink: CocktailType; quantity: number }[];
-    total: number;
+  id?: string;
+  firstName: string;
+  email: string;
+  orderTimestamp: string;
+  time: string;
+  dishes: { dish: DishType; quantity: number }[];
+  drinks: { drink: CocktailType; quantity: number }[];
+  total: number;
+};
+
+const pb = new PocketBase(process.env.NEXT_PUBLIC_DB_HOST);
+
+export async function uploadOrder(
+  order: OrderContextType,
+  orderTimestamp: string,
+  time: string,
+  firstName: string,
+  email: string,
+  total: number
+) {
+  const data: OrderType = {
+    firstName: firstName,
+    email: email,
+    orderTimestamp: orderTimestamp,
+    time: time,
+    dishes: order.dishes,
+    drinks: order.drinks,
+    total: total,
   };
-
-const pb = new PocketBase(process.env.NEXT_PUBLIC_DB_HOST)
-
-export async function uploadOrder(order: OrderContextType, orderTimestamp: string, time: string, firstName: string, email: string, total: number)  {
-    const data: OrderType = {
-        "firstName": firstName,
-        "email": email,
-        "orderTimestamp": orderTimestamp,
-        "time": time,
-        "dishes": order.dishes,
-        "drinks": order.drinks,
-        "total": total
-    }
-    const record = await pb.collection('order').create(data)
+  const record = await pb.collection("order").create(data);
 }
 
 export async function getOrderById(id: string) {
-    const record: OrderType = await pb.collection('order').getOne(id);
-    return record;
+  const record: OrderType = await pb.collection("order").getOne(id);
+  return record;
 }
 
 export async function getLatestOrderByEmail(email: string) {
-    const record: OrderType = await pb.collection('order').getFirstListItem(`email="${email}"`, {sort: '-created'});
-    return record;
+  const record: OrderType = await pb
+    .collection("order")
+    .getFirstListItem(`email="${email}"`, { sort: "-created" });
+  return record;
 }
 
 export async function getAllOrdersByEmail(email: string): Promise<OrderType[]> {
-    const result = await pb.collection('order').getList(1, 100, { filter: `email="${email}"`, sort: '-created'});
-    const records: OrderType[] = result.items.map((item: any) => ({
-        id: item.id,
-        firstName: item.firstName,
-        email: item.email,
-        orderTimestamp: item.orderTimestamp,
-        time: item.time,
-        dishes: item.dishes,
-        drinks: item.drinks,
-        total: item.total
-      }));
-    return records;
-  }
+  const result = await pb
+    .collection("order")
+    .getList(1, 100, { filter: `email="${email}"`, sort: "-created" });
+  const records: OrderType[] = result.items.map((item: any) => ({
+    id: item.id,
+    firstName: item.firstName,
+    email: item.email,
+    orderTimestamp: item.orderTimestamp,
+    time: item.time,
+    dishes: item.dishes,
+    drinks: item.drinks,
+    total: item.total,
+  }));
+  return records;
+}
