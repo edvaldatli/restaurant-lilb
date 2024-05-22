9aa51ca46617e45401ac1aad8a10c7c80ac47832
Eðvald Atli Sigurvaldsson <edvaldatli@hotmail.com>
2024-05-22 21:08:49 +0000

    Update page.tsx

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -1,11 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
-import {
-  OrderType,
-  getAllOrdersByEmail,
-  getLatestOrderByEmail,
-  getOrderById,
-} from "../utils/serverFunctions";
+import { OrderType, getAllOrdersByEmail } from "../utils/serverFunctions";
 
 import { formatDate } from "../utils/dateFormat";
 
@@ -19,13 +14,14 @@ type Props = {
 
 export default function OrderPage({ orderEmail }: Props) {
   const [orders, setOrders] = useState<OrderType[]>();
+  const [selectedOrder, setSelcectedOrder] = useState<OrderType>();
 
   useEffect(() => {
     const fetchOrder = async () => {
       try {
         const orders = await getAllOrdersByEmail("ewq@ewq.is");
-        console.log(orders);
         setOrders(orders);
+        setSelcectedOrder(orders[0]);
       } catch (e) {
         console.error("Error fetching the latest order", e);
       }
@@ -43,28 +39,57 @@ export default function OrderPage({ orderEmail }: Props) {
   }
 
   return (
-    <div className="flex flex-col bg-request-orange w-full h-full rounded-xl p-4 text-white">
-      <h1 className="text-3xl font-bold text-center pb-2">Your order</h1>
-      <div className="flex flex-row w-full justify-center overflow-auto">
-        <ul className="flex flex-col gap-2 w-1/2 h-full">
+    <div className="flex flex-col bg-request-orange w-full h-full rounded-xl text-white">
+      <div className="flex flex-row w-full justify-start">
+        <ul className="flex flex-col w-1/4 h-full rounded-xl">
           {orders.map((order) => (
             <li
-              className="flex flex-col bg-white rounded-xl text-black w-full p-4"
+              className={`"flex flex-col bg-white text-black w-full p-4 border-b-2 hover:bg-zinc-200 transition-colors active:bg-green-50 ${
+                selectedOrder?.id === order.id ? "bg-black" : ""
+              }"`}
               key={order.id}
+              onClick={() => setSelcectedOrder(order)}
             >
               <h2>{order.orderTimestamp}</h2>
               <h3>{formatDate(order.time)}</h3>
               <h3>Total: {order.total}</h3>
-              <li>
-                {order.dishes.map((dish) => (
-                  <li key={dish.dish.id}>
-                    <p>{dish.dish.name}</p>
-                    <p>{dish.dish.price}</p>
-                  </li>
-                ))}
-              </li>
             </li>
           ))}
+        </ul>
+        <ul className="overflow-auto">
+          {selectedOrder && (
+            <li className="p-4">
+              <h2>Pöntun: {formatDate(selectedOrder.orderTimestamp)}</h2>
+              <h3>Bókunar tími: {formatDate(selectedOrder.time)}</h3>
+              <h3>Total: {selectedOrder.total} kr.</h3>
+              <ul>
+                {selectedOrder.dishes.map((item) => (
+                  <li key={item.dish.id} className="">
+                    <h4>{item.dish.name}</h4>
+                    <p>{item.dish.ingredients}</p>
+                    <p>{item.dish.price} kr.</p>
+                  </li>
+                ))}
+                {selectedOrder.drinks.map((item) => (
+                  <li
+                    key={item.drink.idDrink}
+                    className="flex flex-row gap-4 bg-white rounded-xl p-4 text-black h-40"
+                  >
+                    <img
+                      src={item.drink.strDrinkThumb}
+                      alt="Image of drink"
+                      className="rounded-xl"
+                    />
+                    <div>
+                      <h4>{item.drink.strDrink}</h4>
+                      <p>{item.drink.strCategory}</p>
+                      <p>{item.drink.price} kr.</p>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </li>
+          )}
         </ul>
       </div>
     </div>
