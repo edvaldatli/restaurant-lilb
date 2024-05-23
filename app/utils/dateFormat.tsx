986e077f497a055477b25f97a02c12d4bb788992
Eðvald Atli Sigurvaldsson <edvaldatli@hotmail.com>
2024-05-23 01:02:14 +0000

    Update dateFormat.tsx

--- app/utils/dateFormat.tsx
+++ app/utils/dateFormat.tsx
@@ -3,10 +3,10 @@
 import { format } from "date-fns";
 import { is } from "date-fns/locale";
 
-export const formatDate = (dateString: string) => {
-  return format(new Date(dateString), "PP", { locale: is });
+export const formatDate = (date: Date) => {
+  return format(new Date(date), "PP", { locale: is });
 };
 
-export const formatBookingDate = (dateString: string) => {
-  return format(new Date(dateString), "PPpp", { locale: is });
+export const formatBookingDate = (date: Date) => {
+  return format(new Date(date), "PPpp", { locale: is });
 };
